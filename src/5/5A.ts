3f685f6885307ac9dae398f40f6014a5df15203b
Lucas <lucas.leroux@pm.me>
2024-11-03 01:06:29 +0100

    [WIP] still stacks parsing

--- src/5/5A.ts
+++ src/5/5A.ts
@@ -3,32 +3,37 @@ import * as path from "path";
 
 const input = fs.readFileSync(path.resolve(__dirname, "data.txt"), "utf-8");
 
-const crateIndexMap: Record<number, number> = {
-  1: 0,
-  5: 1,
-  9: 2,
-  13: 3,
-  17: 4,
-  21: 5,
-  25: 6,
-  29: 7,
-  33: 8,
-};
+function createCrateIndexToStackMap(length: number) {
+  const map: Record<number, number> = {};
+  for (let i = 0; i < length; i++) {
+    map[1 + 4 * i] = i;
+  }
+  return map;
+}
 
 function getNewTopCrates(input: string) {
   const [startingStacksRawString, procedureRawString] = input.split(/\n\s*\n/);
 
   const stackLines = startingStacksRawString.split(/\n/);
 
-  const startingStacks = new Array(9);
+  const labels = stackLines.pop()?.trim().split(/\s*/);
+  if (labels === undefined) throw new Error("empty stackLines array");
 
-  for (let i = 0; i < stackLines.length - 1; i++) {
+  const startingStacks: Array<Array<string>> = new Array(labels.length)
+    .fill(null)
+    .map(() => []);
+
+  const crateIndexToStackMap = createCrateIndexToStackMap(labels.length);
+
+  for (let i = stackLines.length - 1; i >= 0; i--) {
     const crates = stackLines[i].match(/[A-Z]/g);
+
     if (crates) {
-      for (const crate of crates) {
-        const crateIndex = stackLines[i].indexOf(crate);
-        if (crateIndex in crateIndexMap) {
-          startingStacks[crateIndexMap[crateIndex]] = crate;
+      for (let j = 0; j < crates.length; j++) {
+        const crateIndex = stackLines[i].indexOf(crates[j]); // Fix that, indexOf gets the first matching char it finds
+
+        if (crateIndex in crateIndexToStackMap) {
+          startingStacks[crateIndexToStackMap[crateIndex]].push(crates[j]);
         } else {
           throw new Error(
             `Crate at index ${crateIndex} not found in crateIndexMap`
@@ -38,6 +43,6 @@ function getNewTopCrates(input: string) {
     }
   }
 
-  return stackLines;
+  return startingStacks;
 }
 console.log(getNewTopCrates(input));
