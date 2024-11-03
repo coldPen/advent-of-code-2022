7bd42bacd856c4bef20567beaed0b56477c2ebbb
Lucas <lucas.leroux@pm.me>
2024-11-03 21:37:52 +0100

    Few improvements

--- src/5/5A.ts
+++ src/5/5A.ts
@@ -3,27 +3,19 @@ import * as path from "path";
 
 const input = fs.readFileSync(path.resolve(__dirname, "data.txt"), "utf-8");
 
-const crateIdRegEx = /[A-Z]/g;
+const crateIdRegEx = /[A-Z]/;
 const instructionRegEx = /move\s+(\d+)\s+from\s+(\d+)\s+to\s+(\d+)/;
 
 function getNewTopCrates(input: string) {
   const [stacksRawString, procedureRawString] = input.split(/\n\s*\n/);
 
-  const stacks = parseStacksInput(stacksRawString);
+  const stacks = parseStacks(stacksRawString);
 
   const procedure = parseProcedure(procedureRawString);
 
   for (const { amount, from, to } of procedure) {
-    for (let i = 0; i < amount; i++) {
-      const fromStack = stacks[from - 1];
-      const toStack = stacks[to - 1];
-
-      const topCrate = fromStack.pop();
-      if (topCrate === undefined)
-        throw new Error("No crate to relocate from stack");
-
-      toStack.push(topCrate);
-    }
+    const cratesToMove = stacks[from - 1].splice(-amount).reverse();
+    stacks[to - 1].push(...cratesToMove);
   }
 
   const topCrates = stacks.map((stack) => stack[stack.length - 1]).join("");
@@ -32,7 +24,7 @@ function getNewTopCrates(input: string) {
 }
 console.log(getNewTopCrates(input));
 
-function parseStacksInput(input: string) {
+function parseStacks(input: string) {
   const stackLines = input.split(/\n/);
 
   const labels = stackLines.pop()?.trim().split(/\s*/);
@@ -42,15 +34,17 @@ function parseStacksInput(input: string) {
     .fill(null)
     .map(() => []);
 
-  const crateIndexToStackMap = computeCrateIndexToStackMap(labels.length);
+  const crateIndexToStackMap = getCratePositionMap(labels.length);
 
   for (let i = stackLines.length - 1; i >= 0; i--) {
     for (let j = 0; j < stackLines[i].length; j++) {
       if (crateIdRegEx.test(stackLines[i][j])) {
-        if (j in crateIndexToStackMap) {
+        if (crateIndexToStackMap[j] !== undefined) {
           startingStacks[crateIndexToStackMap[j]].push(stackLines[i][j]);
         } else {
-          throw new Error(`Missing index "${j}" in crateIndexToStackMap`);
+          throw new Error(
+            `Missing index "${j}" for crate "${stackLines[i][j]}" in crateIndexToStackMap`
+          );
         }
       }
     }
@@ -59,7 +53,7 @@ function parseStacksInput(input: string) {
   return startingStacks;
 }
 
-function computeCrateIndexToStackMap(length: number) {
+function getCratePositionMap(length: number) {
   const map: Record<number, number> = {};
   for (let i = 0; i < length; i++) {
     map[1 + 4 * i] = i;
@@ -71,7 +65,9 @@ function parseProcedure(input: string) {
   const procedure = input.split(/\n/).map((line) => {
     const result = line.match(instructionRegEx);
     if (result === null)
-      throw new Error(`"${line}" doesn't match with regular expression`);
+      throw new Error(
+        `"${line}" doesn't match the expected instruction format.`
+      );
     const [, amount, from, to] = result;
     return {
       amount: parseInt(amount),
